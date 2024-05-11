ec239e96aeabc0625a567f627ea447f07e1de057
Tauhid hossen <tauhidhossen04@gmail.com>
2024-05-11 15:55:36 +0600

    get data from mongoDB

--- index.js
+++ index.js
@@ -29,14 +29,25 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
+
+    const recentQueries =client.db('AlternativeProduct').collection('RecentQueries')
+
+
+    //get recentQuries
+    app.get('/RecentQueries',async (req,res)=>{
+
+        const result=await recentQueries.find().toArray()
+
+        res.send(result)
+    })
+
     await client.connect();
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    
   }
 }
 run().catch(console.dir);
